759de863010dbbd7d56a3def6fe30e86fd2c7de0
Roshan Shetty <roshan_shetty@hotmail.com>
2019-02-14 02:24:41 +0000

    hardcode values for now.. will review later

--- vars/awsdockerupload.groovy
+++ vars/awsdockerupload.groovy
@@ -12,16 +12,18 @@ def uploadFile(awsroleArn,imagename) {
                 arn = "${awsroleArn}"
                 echo "role name is: ${arn}"
                 def object1
+                def out
                 def keysList = []
                 keysList = assumeawsRole.getKeys(arn)
                 def AWS_ACCESS_KEY_ID = keysList[0].trim()
                 def AWS_SECURITY_TOKEN = keysList[1].trim()
                 def AWS_SECRET_ACCESS_KEY = keysList[2].trim()
-                def out = sh(returnStdout: true, script: "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN} /usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
+                out = sh(returnStdout: true, script: "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN} /usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
                 }
                 else
                 {
-                def out = sh(returnStdout: true, script: "/usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
+                out = sh(returnStdout: true, script: "/usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
+                echo "Out value is: ${out}"
                 }
                 sh("docker tag my-image:13 559994907943.dkr.ecr.us-east-1.amazonaws.com/rshetty-testing-002:latest")
                 sh("echo $out | sh")
