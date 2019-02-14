4722a9344fe685260e56f101fbd82bdc2c50c9dd
Roshan Shetty <roshan_shetty@hotmail.com>
2019-02-14 02:21:04 +0000

    hardcode values for now.. will review later

--- vars/awsdockerupload.groovy
+++ vars/awsdockerupload.groovy
@@ -17,13 +17,14 @@ def uploadFile(awsroleArn,imagename) {
                 def AWS_ACCESS_KEY_ID = keysList[0].trim()
                 def AWS_SECURITY_TOKEN = keysList[1].trim()
                 def AWS_SECRET_ACCESS_KEY = keysList[2].trim()
-                sh("AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN} /usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
+                def out = sh(returnStdout: true, script: "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_SECURITY_TOKEN=${AWS_SECURITY_TOKEN} /usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
                 }
                 else
                 {
                 sh("/usr/bin/aws --region us-east-1 ecr get-login --no-include-email")
                 }
                 sh("docker tag my-image:13 559994907943.dkr.ecr.us-east-1.amazonaws.com/rshetty-testing-002:latest")
+                sh("echo $out | sh")
                 sh("docker push 559994907943.dkr.ecr.us-east-1.amazonaws.com/rshetty-testing-002:latest")
 }
 
